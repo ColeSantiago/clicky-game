a0974903fd8eef47bf5157916396b097ce3f9dca
Cole Santiago <colesantiago@Coles-MacBook-Pro.local>
2018-04-10 18:16:12 -0400

    created funtion to shuffle the pictures, added click: false to each photo in json

--- src/App.js
+++ src/App.js
@@ -7,18 +7,36 @@ import './App.css';
 
 class App extends Component {
   state = {
-    characters
+    characters,
+    currentScore: 0,
+    topScore: 0,
+    notClickedYet: characters,
+  };
+
+  Shuffle = array => {
+    for (let i = array.length - 1; i > 0; i--) {
+      let j = Math.floor(Math.random() * (i + 1));
+      [array[i], array[j]] = [array[j], array[i]];
+    }
+  };
+
+    updateState = id => {
+    // Filter this.state.friends for friends with an id not equal to the id being removed
+    const batman = this.state.characters.find(character => character.clicked === clicked);
+    // Set this.state.friends equal to the new friends array
+    this.setState({ friends });
   };
 
   render() {
     return (
       <div>
-        {this.state.characters.map(friend => (
+        {this.state.characters.map(character => (
           <GameArea
-            id={friend.id}
-            key={friend.id}
-            name={friend.name}
-            image={friend.image}
+            updateState={this.updateState}
+            id={character.id}
+            key={character.id}
+            name={character.name}
+            image={character.image}
           />
         ))}
       </div>
